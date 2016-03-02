2a371912441caa202e57911f1c8e33323ff20cef
wab <wab2com@gmail.com>
2016-03-02 18:13:28 +0100

    menu item label

--- site/web/app/themes/store/templates/header-store.php
+++ site/web/app/themes/store/templates/header-store.php
@@ -3,11 +3,10 @@
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/logo.png" alt="" height="40px"> <?php bloginfo('name'); ?> <small><?php bloginfo('description'); ?></small> </a>
     <nav class="menu menu--ferdinand pull-right">
         <ul class="menu__list">
-            <li class="menu__item menu__item--current"><a class="menu__link">Home</a></li>
-            <li class="menu__item"><a class="menu__link">Portfolio</a></li>
-            <li class="menu__item"><a class="menu__link">Blog</a></li>
-            <li class="menu__item"><a class="menu__link">Shop</a></li>
-            <li class="menu__item"><a class="menu__link">Contact</a></li>
+            <li class="menu__item menu__item--current"><a class="menu__link">l'activité</a></li>
+            <li class="menu__item"><a class="menu__link">les stores</a></li>
+            <li class="menu__item"><a class="menu__link">les dernières ventes</a></li>
+            <li class="menu__item"><a class="menu__link">contact</a></li>
         </ul>
     </nav>
   </div>
